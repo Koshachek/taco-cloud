4b8ece1e8634ce2280cdd8c208a2cf1527eba32c
Sergei <cot140194@mail.ru>
2022-07-23 22:03:21 +0300

    Подключил H2

--- src/main/java/tacos/model/TacoOrder.java
+++ src/main/java/tacos/model/TacoOrder.java
@@ -6,11 +6,19 @@ import org.hibernate.validator.constraints.CreditCardNumber;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Data
-public class TacoOrder {
+public class TacoOrder implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    private Date placedAt;
+
     @NotBlank(message = "Delivery name is required")
     private String deliveryName;
 
@@ -29,7 +37,7 @@ public class TacoOrder {
     @CreditCardNumber(message = "Not a valid credit card number")
     private String ccNumber;
 
-    @Pattern(regexp = "^(0[1-9]|1[0-2])([\\/])([2-9][0-9])$",
+    @Pattern(regexp = "^(0[1-9]|1[0-2])(\\/)([2-9][0-9])$",
             message = "Must be formatted MM/YY")
     private String ccExpiration;
 
