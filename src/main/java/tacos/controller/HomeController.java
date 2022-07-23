dfc4193cbedda089bc28b1f8185a8c0297c1b13b
Sergei <cot140194@mail.ru>
2022-07-23 20:54:29 +0300

    Базовая версия приложения: сделаны контроллеры и простой фронт

---
+++ src/main/java/tacos/controller/HomeController.java
@@ -0,0 +1,14 @@
+package tacos.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+// он больше не нужен, мы используем класс WebConfig
+//@Controller
+public class HomeController {
+//
+//    @GetMapping("/")
+//    public String home() {
+//        return "home";
+//    }
+}
