d262dc8083ad5b3fb88279ad1805b4c04470d190
Sergei <cot140194@mail.ru>
2022-07-25 02:46:10 +0300

    Подключил репозиторий

--- src/main/java/tacos/web/WebConfig.java
+++ src/main/java/tacos/web/WebConfig.java
@@ -1,4 +1,4 @@
-package tacos.model;
+package tacos.web;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
